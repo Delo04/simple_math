3a010ef88d32508a32dce7965a2bc95aca38d8f2
Mattia De Lorenzi <mattiadelorenzi110804@gmail.com>
2025-03-08 10:02:16 +0100

    first version of simple math -no test

---
+++ simple_math.c
@@ -0,0 +1,10 @@
+/** \file simple_mah.h
+	\brief a toy example of math library
+	\author Mattia De Lorenzi
+*/
+
+#include "simple_maht.h"
+
+float mean (int a, int b{
+	return (a+b)/2;
+}
---
+++ simple_math.h
@@ -0,0 +1,11 @@
+/** \file simple_mah.h
+	\brief a toy example of math library
+	\author Mattia De Lorenzi
+*/
+
+#ifndef _SIMPLEMATH_
+#define _SIMPLEMATH_
+
+float mean (int a, int b);
+
+#endif
